f995ebe8654ecb6ce84e7e133a289d9e7fef9e17
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-17 19:18:18 +0200

    Solve Javascript exercise 02_repeatString
    Modified repeatString.js so that it passes all 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(word, reps) {
+    let i = 0;
+    let result;
+    if (reps > 0) {
+        result = word;
+        while (i < (reps - 1)) {
+            result += word;
+            i++;
+        }
+    }
+    else if (reps == 0) {
+        result = "";
+    }
+    else {
+        result = "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line

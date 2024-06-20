e482a01f8dd355a622f682323b99c9f8bb4281ef
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-20 17:48:47 +0200

    Solve JavaScript exercise 03_reverseString
    Modified reverseString.js so that it passes all 4 test cases.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = ""; //Creates an empty string
+    for (let i = 0; i < text.length; i++) {
+        reverseText += text.charAt(text.length - (i + 1)); //Fills the string character by character from the original text, going backwards.
+    }
+    return reverseText;
 };
 
 // Do not edit below this line

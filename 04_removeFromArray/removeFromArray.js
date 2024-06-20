4ad64b364b0f88e9c776473216bdb09ffbd94a5d
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-20 19:16:40 +0200

    Solve JavaScript exercise 04_removeFromArray
    Modified removeFromArray.js so that the script passes all 8 test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (let i = 0; i < elements.length; i++) {
+        for (let j = array.length - 1; j >= 0; j--) {
+            if (elements[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

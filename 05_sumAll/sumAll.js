ae4c7d15054b9304cbe1895980f42084bc422dbd
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-20 19:51:02 +0200

    Solve JavaScript exercise 05_sumAll
    Modified sumAll.js so that the script passes all 6 test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (firstNum < 0 || !Number.isInteger(firstNum) || secondNum < 0 || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+    if (firstNum > secondNum) {
+        secondNum = [firstNum, firstNum = secondNum][0];
+    }
+    let sum = firstNum;
+    while (firstNum < secondNum) {
+        firstNum++;
+        sum += firstNum;
+    }
+    return sum;
 };
 
 // Do not edit below this line

cc229309ce32b721bbb0f9752c50c3ceab77de96
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-20 21:11:20 +0200

    Solve JavaScript exercise 06 and 07
    Modified leapYears.js and tempConversion.js so that the scripts pass
    all test cases.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  let decimal = Math.round((tempC % 1) * 10) / 10;
+  if (tempC > 0) {
+    tempC = Math.floor(tempC) + decimal;
+  }
+  else {
+    tempC = Math.ceil(tempC) + decimal;
+  }
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = tempC * 9 / 5 + 32;
+  let decimal = Math.round((tempF % 1) * 10) / 10;
+  if (tempF > 0) {
+    tempF = Math.floor(tempF) + decimal;
+  }
+  else {
+    tempF = Math.ceil(tempF) + decimal;
+  }
+  
+  return tempF;
 };
 
 // Do not edit below this line

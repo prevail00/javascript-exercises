9847c707f5490601c29ca8e6050cc6f9d1f6a84e
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-29 11:03:38 +0200

    Solve javascript exercise 09_palindromes
    Modified palindromes.js so that the script passes all test cases.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.replace(/[^a-zA-Z0-9]/g, '');
+    inputString = inputString.toLowerCase();
+    let reverseString = ""; //Creates an empty string
+    for (let i = 0; i < inputString.length; i++) {
+        reverseString += inputString.charAt(inputString.length - (i + 1)); //Fills the string character by character from the original text, going backwards.
+    }
+    if (inputString === reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line

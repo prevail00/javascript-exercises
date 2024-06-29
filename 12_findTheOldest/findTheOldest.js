7c87da76e75bf5950e8677feaf52bb2d05719dd2
prevail00 <113232108+prevail00@users.noreply.github.com>
2024-06-29 13:07:07 +0200

    Solve javascript exercise 12_findTheOldest
    Modified findTheOldest.js so that it passes all test cases.
    Finished all javascript practice exercises.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let oldestPerson;
+    let greatestAge = 0;
+    for (const person of people) {
+        if (person.yearOfDeath === undefined) {
+            person.age = currentYear - person.yearOfBirth;
+        }
+        else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (person.age > greatestAge) {
+            greatestAge = person.age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
